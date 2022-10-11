ae21176dd046fad595002520d8d253a305ae0156
FebrianaPasonangSidauruk <febriana.pasonang@ui.ac.id>
2022-10-11 23:59:32 +0700

    update lagi

--- src/components/Audit/Tes.js
+++ src/components/Audit/Tes.js
@@ -1,24 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import axios from 'axios'
 
 
 const Tes = () => {
     // const [title_dev, setTitle_dev] = useState({title_dev:[]});
     // const[datas, setDatas] = useState({datas:[]});
-    const [model, setmodel] = useState("");
-
     const [title_dev, setTitle_dev] = useState([]);
     const[datas, setDatas] = useState([]);
-var requestor_list;
+    const [model, setmodel] = useState("");
+
+    
+    // var requestor_list;
+
 
     useEffect(() => {
+      // if(datas==[]){
+      //   getProducts();
+      // }
         getProducts();
         // removeDuplicates();
       }, []);
 
+    //   const getProducts = async() => {
+    //     await axios.get(`http://localhost:5001/getProject`).then(data => setDatas(data));
+    //   }
+
+    //   useEffect(()=>{
+    //     if(datas){
+    //         const data_list = datas.map(data => data.title_dev);
+    //     setTitle_dev(data_list)
+    //     }
+    //   },[datas])
+
     const getProducts = async() =>{
         const res = await axios.get(`http://localhost:5001/getProject`);
-        setDatas(res.data);
+        // setDatas(res.data);
+        // console.log(datas);
+
+        const requestor_list = res.data.map((data) => data.title_dev);
+        setTitle_dev([...new Set(requestor_list)]);
+        console.log(title_dev);
+
+        return;
+        // requestor_list = datas.map((data) => data.title_dev);
+        // setTitle_dev([...new Set(requestor_list)]);
         // requestor_list = datas.map((data) => data.title_dev);
         // console.log(datas);
         // requestor_list = datas.map((data) => data.title_dev);
@@ -31,16 +56,15 @@ var requestor_list;
         // requestor_list = [...new Set(title_dev)];
         
     }
-    useEffect(() => {
-        removeDuplicates();
-    })
+    // useEffect(() => {
+    //     removeDuplicates();
+    // })
 
-    const removeDuplicates = async() =>{
-      // console.log(datas);
-      requestor_list = datas.map((data) => data.title_dev);
-      setTitle_dev([...new Set(requestor_list)]);
-      
-  }
+    // const removeDuplicates = async() =>{
+    //     requestor_list = datas.map(data => data.title_dev);
+    //     setTitle_dev(requestor_list)
+
+    // }
 
     // function removeDuplicates(datas){
     //     return datas.filter((value, index) => datas.indexOf(value) === index);
@@ -127,98 +151,3 @@ var requestor_list;
 
 export default Tes
 
-// export default class Inference extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-       
-//         model: "",
- 
-//         options: [
-//           { label: "Cat", value: "cat" },
-//           { label: "Traffic", value: "traffic" },
-//           { label: "Dog", value: "dog" },
-//         ],
-//       };
-//       this.handleChange = this.handleChange.bind(this);
-//     }
-    
-  
-//     handleChange(e) {
-     
-//       this.setState({ model: e.target.value });
-//       console.log("Model Selected!!");
-//     }
-
-//     handleSubmit(event) {
-//       event.preventDefault();
-//       axios.post('http://localhost:5001/getmodel', {
-//         model: this.state.model,
-//       })
-//        .then((res) => {
-//         console.log(res.data);
-//       });
-//     }
-  
-
-  
-//     render() {
-     
-  
-//       const {  options } = this.state;
-  
-   
-  
-//       return (
-//         <div className="container">
-//           <div className="row">
-  
-//             <div
-//               className="col-6"
-//               style={{
-//                 paddingBottom: "100px",
-//                 paddingTop: "20px",
-//                 alignItems: "center",
-//               }}
-//             >
-//               <label
-//                 className=""
-//                 style={{ paddingTop: "5px", marginTop: "40px" }}
-//               >
-//                 Model
-//                 <form onSubmit={this.handleSubmit.bind(this)}>
-//                   <select
-//                     className="custom-select"
-//                     name="example"
-//                     value={this.state.model}
-//                     onChange={this.handleChange}
-//                     style={{ paddingTop: "5px", marginTop: "10px" }}
-//                   >
-//                     <option>--Select--</option>
-//                     {options.map((option) => (
-//                       <option
-//                         value={option.value}
-//                         onChange={(e) => this.setState({ model: e.target.value })}
-//                       >
-//                         {option.label}
-//                       </option>
-//                     ))}
-//                   </select>
-  
-               
-  
-//                   <button
-//                     type="submit"
-//                     class="btn btn-success"
-//                     style={{ marginTop: "100px" }}
-//                   >
-//                     Inference
-//                   </button>
-//                 </form>
-//               </label>
-//             </div>
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
